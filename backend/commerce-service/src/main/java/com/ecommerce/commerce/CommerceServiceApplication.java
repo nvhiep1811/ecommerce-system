4903d95ce01503cc5696e385736ea42e376fa890
nvhiep1811 <22707701.hiep@student.iuh.edu.vn>
2026-05-24 22:18:55 +0700

    feat(events): make Kafka outbox the default backbone

--- backend/commerce-service/src/main/java/com/ecommerce/commerce/CommerceServiceApplication.java
+++ backend/commerce-service/src/main/java/com/ecommerce/commerce/CommerceServiceApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableRabbit
 @EnableKafka
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.ecommerce")
